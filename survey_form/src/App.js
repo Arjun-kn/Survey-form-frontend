1e6c98521b7cc1600f3c1da537bbe589d8aa4265
Kushal-Kake <kakekushalkumar369@gmail.com>
2023-08-11 15:47:15 +0530

    Registration form design

--- survey_form/src/App.js
+++ survey_form/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Login from './components/Login/Login';
 import { BrowserRouter,Routes, Route } from 'react-router-dom';
+import Register from './components/Register/Register';
 
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
 <BrowserRouter>
 <Routes>
 <Route  path="/" element={<Login/>}/>
-
+<Route path="/register" element={<Register />} />
 </Routes>
 </BrowserRouter>
 </>
--- survey_form/src/components/Login/Login.js
+++ survey_form/src/components/Login/Login.js
@@ -1,24 +1,24 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './Login.css'
 import Loginform from '../Loginform/Loginform'
 function Login() {
   return (
     <div className='prnt'>
       <div className='text-container'>
-        <h1>Welcome Page <br/>
-            One line text <br/>
-            Will be here
+        <h1>Welcome Page <br />
+          One line text <br />
+          Will be here
         </h1>
         <h3>Sign in to continue access pages</h3>
         <div className='btmsec'>
-            <p>Don,t Have An Account?</p>
-            <button><Link to="/register" className='lnk'>Register</Link></button>
+          <p>Don,t Have An Account?</p>
+          <button><Link to="/register" className='lnk'>Register</Link></button>
         </div>
       </div>
-   <div className='frm'>
-    <Loginform />
-   </div>
+      <div className='frm'>
+        <Loginform />
+      </div>
     </div>
   )
 }
